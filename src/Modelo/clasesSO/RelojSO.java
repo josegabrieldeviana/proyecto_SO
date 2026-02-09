70ef0a6b98421a69224623c007d35dc615e2a970
josegabrieldeviana <josegabrieldeviana@gmail.com>
2026-02-09 19:27:12 -0400

    implementado ciclos con duración configurable.

--- src/Modelo/clasesSO/RelojSO.java
+++ src/Modelo/clasesSO/RelojSO.java
@@ -6,24 +6,46 @@ package Modelo.clasesSO;
 
 /**
  *
- * Se asume que 1 ciclo de reloj= 1 seg 
+ * Se asume que 1 ciclo de reloj= 1 seg
+ * 
  * @author joseg
  */
 public class RelojSO extends Thread {
-    
-    
+
+    private long sleepTime = 1000; // Default: 1 second
+    private int ciclos = 0;
+    private boolean activo = true;
+
     @Override
-    public void run(){
-        for(int i=0; i > 0; i++){
-            //System.out.println(i);
+    public void run() {
+        while (activo) {
             try {
-                Thread.sleep(1000);
+                Thread.sleep(sleepTime);
+                ciclos++;
+                // System.out.println("Ciclo: " + ciclos);
             } catch (InterruptedException ex) {
                 System.getLogger(RelojSO.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
             }
         }
     }
-    
-    
-    
+
+    public synchronized void setCicloDuracion(long ms) {
+        this.sleepTime = ms;
+    }
+
+    public synchronized long getCicloDuracion() {
+        return this.sleepTime;
+    }
+
+    public synchronized int getCiclos() {
+        return this.ciclos;
+    }
+
+    public synchronized void resetCiclos() {
+        this.ciclos = 0;
+    }
+
+    public void deternereReloj() {
+        this.activo = false;
+    }
 }
